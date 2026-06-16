a4bb5a41fdf49d41770da052ab3fc140df5bba5c
abrilboya-code <abril.boya@estudiantat.upc.edu>
2026-06-16 13:37:36 +0200

    Modify sleep duration and add second trajectory path
    Increased sleep duration between joint movements and added a second trajectory path for the robot.

--- pinça.py
+++ pinça.py
@@ -16,22 +16,31 @@ def send_joint_path(path, sock):
     for joint_config in path:
         print(joint_config)
         sock.send(f"movej({joint_config}, a=0.5, v=0.5)".encode() + "\n".encode())
-        time.sleep(3)
+        time.sleep(10)
 
 # Conexión via socket a la controladora del robot
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock.connect((HOST, PORT))
 
+# Trayectoria -- configuraciones (variables articulares, en radianes)
+path = [
+     [-1.447159069008518, -1.644827512423482, -1.029983530194813, -0.422696837116301, 1.497014895096205 -0.003751004440434],
+    # Añadir más si hiciera falta
+]
+path2 = [
+      [-0.750209626968530, -1.559133993122124, -1.114111288401728, -0.407425412001099, 0.801139545964185, -0.042927069636849],
+    # Añadir más si hiciera falta
+]
+# Se envia la trayectoria a la controladora del robot
+send_joint_path(path, sock)
 # Enviar archivo script abrir pinza
 with open(Cerrar_pinza, 'rb') as f: sock.sendall(f.read())
-time.sleep(0.5)
+time.sleep(3)
 # Se envia la trayectoria a la controladora del robot
-send_joint_path(path, sock)
+send_joint_path(path2, sock)
 # Enviar archivo script cerrar pinza
 with open(Abrir_pinza, 'rb') as f: sock.sendall(f.read())
-time.sleep(0.5)
-# Se envia la trayectoria a la controladora del robot
-send_joint_path(path, sock)
+time.sleep(3)
 
 # Mensaje que se imprime cuando se finaliza la ejecución
 # de la trayectoria
